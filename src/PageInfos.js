c3d60b21b38607c2b5dd2d42d9a282926bcc79e7
MaLrc <102948498+MaLrc@users.noreply.github.com>
2022-08-26 01:30:25 +0200

    Autre version avec affichage du matériel scanné

--- src/PageInfos.js
+++ src/PageInfos.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import JsonData from './services/base_materiels.json';
-
+import _ from 'lodash'
 class PageInfos extends Component {
+// props.id du composant = id matériel
+    constructor(props) {
+        this.props = props
+    }
 
     data_materiels = JSON.parse(JsonData)
+
+    selected_materiel = _.find(this.data_materiels, {id: props.id})
     
     render() {
         return (
@@ -21,22 +27,18 @@ class PageInfos extends Component {
                 <th>Date de garantie</th>
                 </tr>
             </thead>
-            <tbody>{
-                data_materiels.map(({type, dpt, entite, nom_matos, marque, modele, num_serie, date_gtie}) => {
-                    return (
-                        <tr>
-                            <td>{type}</td>
-                            <td>{dpt}</td>
-                            <td>{entite}</td>
-                            <td>{nom_matos}</td>
-                            <td>{marque}</td>
-                            <td>{modele}</td>
-                            <td>{num_serie}</td>
-                            <td>{date_gtie}</td>
-                        </tr>
-                    )
-                })
-                }</tbody>
+            <tbody>
+                <tr>
+                    <td>{this.selected_materiel.type}</td>
+                    <td>{this.selected_materiel.dpt}</td>
+                    <td>{this.selected_materiel.entite}</td>
+                    <td>{this.selected_materiel.nom_matos}</td>
+                    <td>{this.selected_materiel.marque}</td>
+                    <td>{this.selected_materiel.modele}</td>
+                    <td>{this.selected_materiel.num_serie}</td>
+                    <td>{this.selected_materiel.date_gtie}</td>
+                </tr>
+            </tbody>
             </table>
         </div>
         );
